d46227c8f2f3285f043700c55cb31443ee3dcd39
RstProfessional <rsantos.professional@gmail.com>
2023-06-09 17:49:40 +0100

    correct erros

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,9 +1,9 @@
 """
-Test custom Django managment commands.
+Test custom Django management commands.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
