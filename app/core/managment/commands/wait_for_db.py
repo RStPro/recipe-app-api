964ec11909d55df5f7f5fa6f075847dec35795da
RstProfessional <rsantos.professional@gmail.com>
2023-06-09 17:46:59 +0100

    correct errors

--- app/core/managment/commands/wait_for_db.py
+++ app/core/managment/commands/wait_for_db.py
@@ -1,9 +1,11 @@
 """
 Django command to wait for the database to be available.
 """
-from django.core.managment.base import BaseCommand
+from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Django command to wait for database."""
+
     def handle(self, *args, **options):
         pass
