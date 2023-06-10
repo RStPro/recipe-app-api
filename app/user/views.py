fcae32545780d7c5a074b410f78ee60303b0b066
RstProfessional <rsantos.professional@gmail.com>
2023-06-10 17:21:03 +0100

    correct some errors

--- app/user/views.py
+++ app/user/views.py
@@ -7,15 +7,17 @@ from rest_framework.settings import api_settings
 
 from user.serializers import (
     UserSerializer,
-    AuthTokenSerializer
-    )
+    AuthTokenSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
 
+
 class CreateTokenView(ObtainAuthToken):
-    """Create a new auth token for user"""
+    """Create a new auth token for user."""
+
     serializer_class = AuthTokenSerializer
-    renderer_class = api_settings.DEFAULT_RENDERER_CLASSES
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
